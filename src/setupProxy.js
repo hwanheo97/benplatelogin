8c524f39ff575ea45d44fea27cb8860c5db6f42d
hwanheo <38911266+hwanheo97@users.noreply.github.com>
2021-12-30 16:20:47 +0900

    add setupProxy

---
+++ src/setupProxy.js
@@ -0,0 +1,17 @@
+const {createProxyMiddleware } = require('http-proxy-middleware');
+//proxy로 대체 가능 <= { createProxyMiddleware }
+module.exports = function(app) {
+   
+  app.use(
+    '/api',
+    createProxyMiddleware({
+     //proxy로 대체 가능 <= { createProxyMiddleware }
+   // target: 'http://localhost:5000',    //local
+    //  target: 'http://3.139.239.112/process.env.PORT || 5000',    //deploy aws okay
+      target: 'https://benplate.herokuapp.com', 
+      changeOrigin: true,
+      
+    })
+  );
+  
+};
